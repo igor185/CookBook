b444cdc00014cbf022d73ed49acf52f3f495fdd7
igor185 <igor.babin185@gmail.com>
2019-09-27 22:12:06 +0300

    extend seed recipe

--- server/src/seed/index.ts
+++ server/src/seed/index.ts
@@ -1,7 +1,9 @@
 import {Recipe1569435351715} from './1569435351715-Recipe'
+import {MoreRecipe1569610778683} from "./1569610778683-MoreRecipe";
 
 // typeorm migration:create -n <name>
 
 export default[
-    Recipe1569435351715
+    Recipe1569435351715,
+    MoreRecipe1569610778683
 ]
